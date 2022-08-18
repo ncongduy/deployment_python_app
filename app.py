b9565523b3ef1fc77ffffdf6b1a9df532223ef98
Nguyen Cong Duy <ncongduy@gmail.com>
2022-08-18 17:25:53 +0300

    feat: first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Home page'
+
+@app.route('/forum', methods=['GET','POST', 'PUT', 'DELETE'])
+def forum():
+    if request.method == 'POST':
+        return 'Accepted'
+    elif request.method == 'PUT':
+        return 'Updated'
+    elif request.method == 'DELETE':
+        return 'Deleted'
+    else:
+        return 'Show articles'
+
+app.run()
